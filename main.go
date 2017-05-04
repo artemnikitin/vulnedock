4b97fc2655fd2d7047174f8b1c27b878e6257ccf
Artem Nikitin <artemnikitin@artemnikitin.com>
2017-05-04 21:01:41 +0200

    Initial implementation

---
+++ main.go
@@ -0,0 +1,225 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/docker/docker/api/types"
+	"github.com/moby/moby/client"
+)
+
+const URL = "https://vulners.com/api/v3/audit/audit/"
+
+var (
+	OSVersion      = []string{"cat", "/etc/os-release"}
+	UbuntuPackages = []string{"dpkg-query", "-W", "-f='${Package} ${Version} ${Architecture}\n'"}
+	CentOSPackages = []string{"rpm", "-qa"}
+	AlpinePackages = []string{"apk", "-v", "info"}
+	UbuntuOS       = []string{"debian", "ubuntu", "kali"}
+	CentOS         = []string{"rhel", "centos", "oraclelinux", "suse", "fedora"}
+	AlpineOS       = []string{"alpine"}
+)
+
+// RequestBody describe JSON for request
+type RequestBody struct {
+	Os      string   `json:"os"`
+	Version string   `json:"version"`
+	Package []string `json:"package"`
+}
+
+// ResponseBody contains response from vulners.com
+type ResponseBody struct {
+	Result string `json:"result"`
+	Data   struct {
+		Error           string   `json:"error"`
+		ErrorCode       int      `json:"errorCode"`
+		Vulnerabilities []string `json:"vulnerabilities"`
+		Reasons         []struct {
+			Package         string `json:"package"`
+			ProvidedVersion string `json:"providedVersion"`
+			BulletinVersion string `json:"bulletinVersion"`
+			ProvidedPackage string `json:"providedPackage"`
+			BulletinPackage string `json:"bulletinPackage"`
+			Operator        string `json:"operator"`
+			BulletinID      string `json:"bulletinID"`
+		} `json:"reasons"`
+		Cvss struct {
+			Score  int    `json:"score"`
+			Vector string `json:"vector"`
+		} `json:"cvss"`
+		Cvelist []string `json:"cvelist"`
+		ID      string   `json:"id"`
+	} `json:"data"`
+}
+
+func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	ctx := context.Background()
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resp, err := cli.ContainerList(ctx, types.ContainerListOptions{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, v := range resp {
+		getInfo(cli, ctx, v)
+	}
+}
+
+func getInfo(cli *client.Client, ctx context.Context, container types.Container) {
+	osver := executeCmd(cli, ctx, container.ID, OSVersion)
+
+	var pkgs []string
+	if checkOS(osver, UbuntuOS) {
+		temp := executeCmd(cli, ctx, container.ID, UbuntuPackages)
+		pkgs = strings.Split(temp, "\n")
+	} else if checkOS(osver, CentOS) {
+		temp := executeCmd(cli, ctx, container.ID, CentOSPackages)
+		pkgs = strings.Split(temp, "\n")
+	} else if checkOS(osver, AlpineOS) {
+		temp := executeCmd(cli, ctx, container.ID, AlpinePackages)
+		pkgs = strings.Split(temp, "\n")
+	} else {
+		log.Fatal("Can't determine type of OS or OS is not supported...")
+	}
+
+	name, ver := getOSNameAndVersion(osver)
+	fmt.Println("For OS:", name+" "+ver)
+	//fmt.Println(name + " " + ver)
+	body := &RequestBody{
+		Os:      name,
+		Version: ver,
+		Package: pkgs,
+	}
+	_, err := getVulnerabilities(body)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func checkOS(text string, options []string) bool {
+	var res bool
+	for _, v := range options {
+		if strings.Contains(strings.ToLower(text), v) {
+			res = true
+			break
+		}
+	}
+	return res
+}
+
+func getOSNameAndVersion(text string) (string, string) {
+	var name string
+	var version string
+
+	if i := strings.Index(text, "NAME="); i > -1 {
+		temp := text[i+6:]
+		i := strings.Index(temp, "\"")
+		name = temp[:i]
+
+	}
+	if i := strings.Index(text, "VERSION_ID="); i > -1 {
+		temp := text[i+11:]
+		if string(temp[0]) == "\"" {
+			i := strings.Index(temp[1:], "\"")
+			version = temp[1 : i+1]
+		} else {
+			i := strings.Index(temp, "\n")
+			version = temp[:i]
+		}
+	}
+
+	return name, version
+}
+
+func executeCmd(cli *client.Client, ctx context.Context, ID string, cmd []string) string {
+	params := types.ExecConfig{
+		AttachStderr: true,
+		AttachStdout: true,
+		Tty:          true,
+		Cmd:          cmd,
+	}
+
+	resp, err := cli.ContainerExecCreate(ctx, ID, params)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//fmt.Println(resp)
+
+	hijack, err := cli.ContainerExecAttach(ctx, resp.ID, params)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer hijack.Close()
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(hijack.Reader)
+	return buf.String()
+}
+
+func getVulnerabilities(rb *RequestBody) ([]string, error) {
+	client := http.Client{
+		Timeout: 30 * time.Second,
+	}
+
+	data, err := json.Marshal(rb)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest(http.MethodPost, URL, bytes.NewBuffer(data))
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		io.Copy(ioutil.Discard, resp.Body)
+		resp.Body.Close()
+	}()
+
+	data, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	body := &ResponseBody{}
+	err = json.Unmarshal(data, body)
+	if err != nil {
+		return nil, err
+	}
+
+	return extractVulnerabilitiesFromResponse(body), nil
+}
+
+func extractVulnerabilitiesFromResponse(body *ResponseBody) []string {
+	var result []string
+	if body.Result != "OK" {
+		fmt.Println("Importance:", body.Data.Cvss.Score)
+		fmt.Println("List of CVE:")
+		for _, v := range body.Data.Cvelist {
+			fmt.Println(v)
+		}
+		fmt.Println("List of Bulletin ID:")
+		for _, v := range body.Data.Reasons {
+			fmt.Println(v.BulletinID)
+		}
+	}
+	return result
+}
